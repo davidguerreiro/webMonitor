dc2f9c6f266ccc3c529cdf9565c0ce9d8a4eec43
davidguerreiro <me@davidguerreiro.com>
2024-03-30 14:39:12 +0100

    feat: complete css code for footer desktop layout

--- template-parts/footer/main-footer.php
+++ template-parts/footer/main-footer.php
@@ -15,8 +15,11 @@ $logo_url = get_template_directory_uri() . '/app/images/logos/logo.png';
     <div class="main-footer__logo">
         <img src="<?php echo esc_url($logo_url); ?>" alt="">
     </div>
-    <p class="main-footer__text-content">
-        Jesús Giráldez Vaquero &copy; 2024
+    <p class="main-footer__main-name">
+        Jesús Giráldez Vaquero
+    </p>
+    <p class="main-footer__disclaimer">
+        &copy; 2024 Todos los derechos reservados. Web desarrollada por David Guerreiro.
     </p>
 </div>
 
