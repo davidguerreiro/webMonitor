cffc4e7d5d8eb28701e4dfcde3192f1460b3f7ce
davidguerreiro <me@davidguerreiro.com>
2024-03-30 20:42:42 +0100

    feat: complete php code for masters section

--- template-parts/home/home-masters.php
+++ template-parts/home/home-masters.php
@@ -7,58 +7,49 @@
  * @author David Guerreiro
  */
 
- $maestro_one_url = get_template_directory_uri() . '/app/images/content/maestro-1.jpeg';
- $maestro_two_url = get_template_directory_uri() . '/app/images/content/maestro-2.jpeg';
+ $title     = get_field('home_masters_section_title') ?? '';
+ $masters   = get_field('home_masters_masters') ?? []; 
 
-?>
+if (! empty($masters)): ?>
 
-<section class="home-masters full-width-module">
-    <h2 class="section-title">Maestros</h2>
+    <section class="home-masters full-width-module">
+        <?php if ($title) : ?>
+            <h2 class="section-title"><?php echo esc_html($title); ?></h2>
+        <?php endif; 
+        
+        for ($i = 0; $i < count($masters); $i++) : 
+            $even = ($i % 2 == 0); 
+        ?>
+        
+            <div class="home-masters__section-wrapper <?php echo (! $even) ? 'home-masters__section-wrapper--alternative' : ''; ?>">
+                <div class="home-masters__section <?php echo (! $even) ? 'home-masters__section--alternative' : ''; ?>">
+ 
+                    <div class="home-masters__image <?php echo (! $even) ? 'home-masters__image--alternative' : ''; ?>">
+                        <?php if ($masters[$i]['imagen']) : ?>
+                            <img src="<?php echo esc_url($masters[$i]['imagen']); ?>" alt="">
+                        <?php endif; ?>
+                    </div>
+                    <div class="home-masters__content <?php echo (! $even) ? 'home-masters__content--alternative' : ''; ?>">
+                        <h3 class="home-masters__content-title"><?php echo esc_html($masters[$i]['nombre']); ?></h3>
 
-    <div class="home-masters__section-wrapper">
-        <div class="home-masters__section">
-            <div class="home-masters__image">
-                <img src="<?php echo esc_url($maestro_one_url); ?>" alt="">
+                        <?php if ($masters[$i]['disciplinas']) : ?>
+                            <ul class="home-masters__list-content">
+                                <?php foreach ($masters[$i]['disciplinas'] as $disciplina) : ?>
+                                    <li>
+                                        <?php echo esc_html($disciplina); ?>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        <?php endif; ?>
+
+                        <div class="home-masters__description">
+                            <?php echo $masters[$i]['contenido']; ?>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="home-masters__content">
-                <h3 class="home-masters__content-title">Mi Shifu</h3>
-                <ul class="home-masters__list-content">
-                    <li>
-                        <?php _e('Disciplina:', 'webJesus'); ?> <span>Disciplina 1</span>
-                    </li>
-                </ul>
-                <p class="home-masters__description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at lectus velit. Suspendisse ut tristique lectus. Nunc rhoncus risus a tellus imperdiet porttitor sed sit amet lacus. Proin porta lacus consectetur facilisis aliquam. Maecenas cursus quam sit amet velit pulvinar convallis. Curabitur tincidunt tincidunt suscipit.
-                </p>
-                
-                <p class="home-masters__description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at lectus velit. Suspendisse ut tristique lectus. Nunc rhoncus risus a tellus imperdiet porttitor sed sit amet lacus. Proin porta lacus consectetur facilisis aliquam. Maecenas cursus quam sit amet velit pulvinar convallis. Curabitur tincidunt tincidunt suscipit.
-                </p>
-            </div>
-        </div>
-    </div>
 
-    <div class="home-masters__section-wrapper home-masters__section-wrapper--alternative">
-        <div class="home-masters__section home-masters__section--alternative">
-            <div class="home-masters__image home-masters__image--alternative">
-                <img src="<?php echo esc_url($maestro_two_url); ?>" alt="">
-            </div>
-            <div class="home-masters__content home-masters__content--alternative">
-                <h3 class="home-masters__content-title">Qihui Zhu</h3>
-                <ul class="home-masters__list-content">
-                    <li>
-                        <?php _e('Disciplina:', 'webJesus'); ?> <span>Disciplina 1</span>
-                    </li>
-                </ul>
-                <p class="home-masters__description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at lectus velit. Suspendisse ut tristique lectus. Nunc rhoncus risus a tellus imperdiet porttitor sed sit amet lacus. Proin porta lacus consectetur facilisis aliquam. Maecenas cursus quam sit amet velit pulvinar convallis. Curabitur tincidunt tincidunt suscipit.
-                </p>
-                
-                <p class="home-masters__description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at lectus velit. Suspendisse ut tristique lectus. Nunc rhoncus risus a tellus imperdiet porttitor sed sit amet lacus. Proin porta lacus consectetur facilisis aliquam. Maecenas cursus quam sit amet velit pulvinar convallis. Curabitur tincidunt tincidunt suscipit.
-                </p>
-            </div>
-        </div>
-    </div>
+        <?php endfor; ?>
 
-</section>
+    </section>
+<?php endif;
