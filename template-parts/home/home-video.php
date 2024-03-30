eafe64fb95342f2657052b9ef8907c2a8aac4b12
davidguerreiro <me@davidguerreiro.com>
2024-03-30 21:02:00 +0100

    feat: complete php code for video module

--- template-parts/home/home-video.php
+++ template-parts/home/home-video.php
@@ -7,19 +7,27 @@
  * @author David Guerreiro
  */
 
-$video = get_template_directory_uri() . '/app/videos/video-jesus.mp4';
+$title      = get_field('home_video_title') ?? '';
+$video      = get_field('home_video_video') ?? [];
+$caption    = get_field('home_video_caption') ?? '';
 
-?>
+if (! empty($video)) : ?>
 
 <section class="home-video">
-    <h2 class="section-title">Video</h2>
+    <?php if ($title) : ?>
+        <h2 class="section-title"><?php echo esc_html($title); ?></h2>
+    <?php endif; ?>
 
     <div class="home-video__wrapper">
         <video controls muted>
-            <source src="<?php echo esc_url($video); ?>" type="video/mp4">
+            <source src="<?php echo esc_url($video['url']); ?>" type="<?php echo esc_html($video['mime_type']); ?>">
         </video>
-        <p class="home-video__video-desc">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at lectus velit. Suspendisse ut tristique lectus. Nunc rhoncus risus a tellus imperdiet porttitor sed sit amet lacus.
-        </p>
+
+        <?php if ($caption) : ?>
+            <p class="home-video__video-desc">
+                <?php echo esc_html($caption); ?>
+            </p>
+        <?php endif; ?>
     </div>
 </section>
+<?php endif;
