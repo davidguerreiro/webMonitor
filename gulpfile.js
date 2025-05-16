1c6ee6b92657e3db3ab424188f9790c74e656ce0
David Guerreiro <me@davidguerreiro.com>
2025-05-16 15:56:50 +0100

    feat: add libro jesus image. Add watcher to gulpfile and create header book banner module

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
 
+// compile sass.
 gulp.task('sass', function() {
     return gulp.src('app/scss/style.scss')
     .pipe(sass())       // using gulp sass.
     .pipe(gulp.dest('.'))
+});
+
+// watch task.
+gulp.task('watch', function() {
+    gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
 });
---
+++ template-parts/header/header-book-banner.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Header top bar module.
+ * 
+ * @package template-parts/header
+ * @author David Guerreiro
+ */
+
+ ?>
+
+<div class="header-book-banner">
+    <div class="header-book-element">
+        <span>"Introduce un texto aqui"</span>
+    </div>
+    <div class="header-book-element">
+        <a href="https://www.caligramaeditorial.com/libro/El-despertar-dragon?isbn=9791387602420" class="header-link-button">Ver libro</a>
+    </div>
+</div>
