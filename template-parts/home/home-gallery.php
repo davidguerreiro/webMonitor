1efb281e35cbc256076f6f6c1998d5cc346facd6
David Guerreiro <me@davidguerreiro.com>
2025-10-11 13:45:23 +0100

    feat: complete PHP code for home gallery module

--- template-parts/home/home-gallery.php
+++ template-parts/home/home-gallery.php
@@ -7,30 +7,41 @@
  * @author David Guerreiro
  */
 
-$images_section_1 = [get_template_directory_uri() . '/app/images/gallery/picture1.jpg', get_template_directory_uri() . '/app/images/gallery/picture2.jpg', get_template_directory_uri() . '/app/images/gallery/picture3.jpg'];
-$images_section_2 = [get_template_directory_uri() . '/app/images/gallery/picture4.jpg', get_template_directory_uri() . '/app/images/gallery/picture5.jpg', get_template_directory_uri() . '/app/images/gallery/picture6.jpg'];
+$title      = get_field('home_gallery_title') ?? '';
+$content_1  = get_field('home_gallery_text_content_1') ?? '';
+$content_2  = get_field('home_gallery_text_content_2') ?? '';
+$gallery    = get_field('home_gallery_gallery') ?? [];
+
+$first_gallery  = array_slice($gallery, 0, 3);
+$second_gallery = array_slice($gallery, 3, 3);
 
 ?>
 
 <section class="home-gallery">
-    <h2 class="section-title">Viaje a China</h2>
-    <p class="home-gallery__content">
-        En agosto de 2025 viajé a China formando parte de la delegación de la escuela Wujia Shaolin Chan Canary Center.
-        En este viaje tuvimos la oportunidad de practicar y obtener varios certificados de maestros de las artes marciales chinas. Uno de ellos de la Chinese Wushu Association de Chongqing y otro de Emei Zhongfeng Martial Arts en Emeishan.
-    </p>
-    <p class="home-gallery__content">
-        Durante el viaje pudimos disfrutar de la cultura china en su propia esencia. Visitando lugares emblemáticos como la Muralla China, la Ciudad Prohibida y el Monte sagrado de Emei. 
-        Para completar el viaje acudimos a actividades tradicionales como la caligrafía china y la ceremonia del té.
-    </p>
+    <?php if ($title) : ?>
+        <h2 class="section-title"><?php echo esc_html($title); ?></h2>
+    <?php endif; 
+    
+    if ($content_1) : ?>
+        <p class="home-gallery__content">
+            <?php echo esc_html($content_1); ?>
+        </p>
+    <?php endif; 
+    
+    if ($content_2) : ?>
+        <p class="home-gallery__content">
+            <?php echo esc_html($content_2); ?>
+        </p>
+    <?php endif; ?>
 
     <div class="home-gallery__grid-wrapper">
         <div class="gallery-grid">
             <div class="gallery-grid__left-image">
-                <img src="<?php echo esc_url($images_section_1[0]); ?>" alt="">
+                <img src="<?php echo esc_url($first_gallery[0]); ?>" alt="">
             </div>
             <div class="gallery-grid__right-images">
-                <img src="<?php echo esc_url($images_section_1[1]); ?>" alt="">
-                <img src="<?php echo esc_url($images_section_1[2]); ?>" alt="">
+                <img src="<?php echo esc_url($first_gallery[1]); ?>" alt="">
+                <img src="<?php echo esc_url($first_gallery[2]); ?>" alt="">
             </div>
         </div>
 
@@ -38,11 +49,11 @@ $images_section_2 = [get_template_directory_uri() . '/app/images/gallery/picture
 
         <div class="gallery-grid">
             <div class="gallery-grid__right-images">
-                <img src="<?php echo esc_url($images_section_2[1]); ?>" alt="">
-                <img src="<?php echo esc_url($images_section_2[2]); ?>" alt="">
+                <img src="<?php echo esc_url($second_gallery[1]); ?>" alt="">
+                <img src="<?php echo esc_url($second_gallery[2]); ?>" alt="">
             </div>
             <div class="gallery-grid__left-image">
-                <img src="<?php echo esc_url($images_section_2[0]); ?>" alt="">
+                <img src="<?php echo esc_url($second_gallery[0]); ?>" alt="">
             </div>
         </div>
     </div>
